cfa76b60502a0baf4f00777b06335e2f7a860dc4
laveberry <73927366+laveberry@users.noreply.github.com>
2023-02-16 10:54:17 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.44 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -2,16 +2,17 @@ import java.util.*;
 
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        String[] reString = my_string.toLowerCase().split("");
+//         String answer = "";
+//         String[] reString = my_string.toLowerCase().split("");
+//         //오름차순 정렬
+//         Arrays.sort(reString);
+//         for(String a : reString){
+//             answer+= a;
+//         }
+//         return answer;
         
-        //오름차순 정렬
-        Arrays.sort(reString);
-        
-        for(String a : reString){
-            answer+= a;
-        }
-        
-        return answer;
+        char[] c = my_string.toLowerCase().toCharArray();
+        Arrays.sort(c);
+        return new String(c);
     }
 }
