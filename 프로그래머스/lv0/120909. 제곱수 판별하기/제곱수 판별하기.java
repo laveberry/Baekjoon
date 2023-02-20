c9ddb64108284c523886f9d9d000964d6600811e
laveberry <73927366+laveberry@users.noreply.github.com>
2023-02-20 17:21:41 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,14 +1,17 @@
 class Solution {
     public int solution(int n) {
-        int answer = 2;
-        
-        for(int i=2 ; i< n/2 ; i++){
-            if(i*i==n){
-                answer = 1;
-                break;
-            }
+        // int answer = 2;
+        // for(int i=2 ; i< n/2 ; i++){
+        //     if(i*i==n){
+        //         answer = 1;
+        //         break;
+        //     }
+        // }
+        // return answer;
+         if (n % Math.sqrt(n) == 0) {
+            return 1;
+        } else {
+            return 2;
         }
-        
-        return answer;
     }
 }
