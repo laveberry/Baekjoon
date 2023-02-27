7e9249765274e1c3b245305c28b057f62f761883
laveberry <73927366+laveberry@users.noreply.github.com>
2023-02-27 10:36:09 +0900

    [level 0] Title: 자릿수 더하기, Time: 1.33 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,16 +1,18 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int n) {
-        int answer = 0;
+         // int answer = 0;
         
-        String[] stn = String.valueOf(n).split("");
+//         String[] stn = String.valueOf(n).split("");
         
-        for(int i=0 ; i<stn.length ; i++){
-            answer += Integer.parseInt(stn[i]);
-        }
+//         for(int i=0 ; i<stn.length ; i++){
+//             answer += Integer.parseInt(stn[i]);
+//         }
         
+        // return answer;
+                
+     return Arrays.stream(String.valueOf(n).split("")).mapToInt(Integer::parseInt).sum();
         
-        
-        
-        return answer;
     }
 }
