5c427fc0167566c93ff349f2663f67b09b81887a
laveberry <73927366+laveberry@users.noreply.github.com>
2023-03-03 17:27:39 +0900

    [level 0] Title: 편지, Time: 0.03 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120898. 편지/편지.java
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(String message) {
-        String[] str = message.split("");
-        return str.length*2;
+        // String[] str = message.split("");
+        // return str.length*2;
+        return message.length()*2;
     }
 }
