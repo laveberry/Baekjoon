f01f965477436c201d2a20dbdadf72e61e78c882
laveberry <73927366+laveberry@users.noreply.github.com>
2023-03-08 17:33:26 +0900

    [level 0] Title: 369게임, Time: 0.02 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.java
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -1,12 +1,20 @@
 class Solution {
     public int solution(int order) {
-        int answer = 0;
-        for(String a : String.valueOf(order).split("")){
-            if(a.equals("3") || a.equals("6") || a.equals("9")){
-                answer++;
-            }
-        }
+         int answer = 0;
+        // for(String a : String.valueOf(order).split("")){
+        //     if(a.equals("3") || a.equals("6") || a.equals("9")){
+        //         answer++;
+        //     }
+        // }    
+        // return answer;
         
+        int count = 0;
+        while(order != 0){
+            if(order % 10 == 3 || order % 10 == 6 || order % 10 == 9) count++;
+             order = order/10;
+        }
+        answer = count;
         return answer;
+        
     }
 }
