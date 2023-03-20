59f3324ff2089cc4d45fcfc6b3b0280b43bf910a
laveberry <73927366+laveberry@users.noreply.github.com>
2023-03-20 20:52:53 +0900

    [level 0] Title: 모음 제거, Time: 0.11 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -1,23 +1,22 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
+        // String answer = "";
+        // String[] str = my_string.split("");
+        // for(String a : str){
+        //     answer += chkStr(a);
+        // }
+        // return answer;
         
-        String[] str = my_string.split("");
-        
-        for(String a : str){
-            answer += chkStr(a);
-        }
-        
-        return answer;
+        return my_string.replaceAll("[aeiou]", "");
     }
     
-    public String chkStr(String str){
-        switch(str){
-            case "a": case "e": case "i": case "o": case "u":
-                return "";
-            default: 
-                return str;
+//     public String chkStr(String str){
+//         switch(str){
+//             case "a": case "e": case "i": case "o": case "u":
+//                 return "";
+//             default: 
+//                 return str;
 
-        }
-    }
+//         }
+//     }
 }
