89f16967a1102c15ab52a07833224f487c6552c8
laveberry <73927366+laveberry@users.noreply.github.com>
2023-04-02 08:46:18 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.39 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        Arrays.sort(numbers);
+        
+        int num1 = numbers[numbers.length-1] * numbers[numbers.length-2];
+        int num2 = numbers[0] * numbers[1];
+
+        answer = num1 > num2 ? num1 : num2;
+        
+        return answer;
+    }
+}
