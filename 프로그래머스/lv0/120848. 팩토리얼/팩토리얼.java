083b51eedacc75e6e63d5adae4cbf62d68be6f93
laveberry <73927366+laveberry@users.noreply.github.com>
2023-04-05 21:27:20 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
@@ -1,13 +1,12 @@
 class Solution {
     public int solution(int n) {
         int answer = 1;
-        
-        for(int i=2 ; i<=10 ; i++){
-            answer*=i;
-            if(answer==n) return i;
-            else if (answer>n) return i-1;
+        int factorial = 1;
+
+        while(n >= factorial) {
+            answer ++;
+            factorial *= answer;
         }
-        
-        return answer;
+        return answer -1 ;
     }
 }
