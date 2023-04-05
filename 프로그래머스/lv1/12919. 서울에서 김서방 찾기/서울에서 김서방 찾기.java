632ce00966c9f609bc08a9bbb29bcba0799a62bf
laveberry <73927366+laveberry@users.noreply.github.com>
2023-04-05 20:44:42 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 7.39 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,18 +1,9 @@
+import java.util.Arrays;
+
 class Solution {
     public String solution(String[] seoul) {
-        String answer = "김서방은 ";
-        int num = 0;
-        
-        for(int i=0 ; i<seoul.length ; i++){
-            if(seoul[i].equals("Kim")){
-                num = i;
-                break;
-            }
-        }
-        
-        answer+= String.valueOf(num);
-        answer+= "에 있다";
-        
-        return answer;
+        int x = Arrays.asList(seoul).indexOf("Kim");
+        return "김서방은 "+ x + "에 있다";
+
     }
 }
