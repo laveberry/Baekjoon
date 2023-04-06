a9de66d85a54d423e0fb79d1878de83243da6674
laveberry <73927366+laveberry@users.noreply.github.com>
2023-04-07 06:09:37 +0900

    [level 1] Title: 음양 더하기, Time: 0.04 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -3,11 +3,7 @@ class Solution {
         int answer = 0;
         
         for(int i=0 ; i<absolutes.length ; i++){
-            if(signs[i]){
-                answer += absolutes[i];
-            }else{
-                answer -= absolutes[i];
-            }  
+            answer += absolutes[i] * (signs[i]? 1: -1); 
         }
         
         return answer;
