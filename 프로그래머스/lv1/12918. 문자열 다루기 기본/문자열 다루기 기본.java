c686a424de7d514b2823c5fd2308ad745ee0bebb
laveberry <73927366+laveberry@users.noreply.github.com>
2023-04-11 21:00:32 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.13 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,15 +1,18 @@
 class Solution {
     public boolean solution(String s) {
-        boolean answer = true;
+//         boolean answer = true;
         
-        if(s.length()==4 || s.length()==6){
-            for(char a : s.toCharArray()){
-                if(!(a>='0'&&a<='9')) return false;
-            }
-        }else{
-            answer = false;
-        }
+//         if(s.length()==4 || s.length()==6){
+//             for(char a : s.toCharArray()){
+//                 if(!(a>='0'&&a<='9')) return false;
+//             }
+//         }else{
+//             answer = false;
+//         }
         
-        return answer;
+//         return answer;
+        
+        return (s.length() != 4 && s.length() != 6) || (s.split("[0-9]").length > 0) ? false:true;
+
     }
 }
