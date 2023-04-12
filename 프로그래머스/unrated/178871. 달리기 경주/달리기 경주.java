62d9859b9b046ec5e5d839e5b42f3e83e905ff37
laveberry <73927366+laveberry@users.noreply.github.com>
2023-04-12 14:27:31 +0900

    [unrated] Title: 달리기 경주, Time: 0.07 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
@@ -4,22 +4,24 @@ class Solution {
     public String[] solution(String[] players, String[] callings) {
         String[] answer = players;
         
-        List<String> arr = new ArrayList<String>();
+        //List<String> arr = new ArrayList<String>();
+        Map<String, Integer> arr = new HashMap<>();
+        int count = 0;
         
-        for(String a : answer) arr.add(a);
+        for(String a : answer) arr.put(a, count++); //arr.add(a);
         
         for(int i=0 ; i<callings.length ; i++){
-            int a = arr.indexOf(callings[i]);
+            //int a = arr.indexOf(callings[i]);
+            int a = arr.get(callings[i]);
             String temp = answer[a-1];
             answer[a-1] = answer[a];
             answer[a] = temp;
-            //배열 순서 바꾸기
-            Collections.swap(arr, a-1,a);
+            arr.put(answer[a-1], a-1);
+            arr.put(temp, a);
+            //시간초과
+            //Collections.swap(arr, a-1,a);
         }
-        
-        
-        
-        
+
         return answer;
     }
 }
