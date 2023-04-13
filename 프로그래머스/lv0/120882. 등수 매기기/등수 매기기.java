b4c4f68c83acd18882837fb59bdde0f27826905f
laveberry <73927366+laveberry@users.noreply.github.com>
2023-04-13 14:39:52 +0900

    [level 0] Title: 등수 매기기, Time: 0.41 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
@@ -1,20 +1,20 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[][] score) {
-        int[] sum = new int[score.length];
+        List<Integer> sum = new ArrayList<>();
         for(int i=0 ; i<score.length ; i++){
-            sum[i] = score[i][0] + score[i][1];
+            sum.add(score[i][0] + score[i][1]);
+        }
+        //리스트 내림차순 정렬
+        sum.sort(Comparator.reverseOrder());
+        
+        int[] answer = new int[score.length];
+        
+        for(int i=0 ; i<score.length ; i++){
+            answer[i] = sum.indexOf(score[i][0] + score[i][1])+1;
         }
             
-        int[] answer = new int[score.length];
-
-        for(int i=0 ; i<score.length ; i++){
-            int num = 1;
-            for(int j=0 ; j<score.length ; j++){
-                if(sum[i]<sum[j]) num++;
-            }
-            answer[i] = num; 
-        }
-        
         return answer;
     }
 }
