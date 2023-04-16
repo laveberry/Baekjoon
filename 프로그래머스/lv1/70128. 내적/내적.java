898afa2b40cab4ea291042bf62a3c100bb51b38a
laveberry <73927366+laveberry@users.noreply.github.com>
2023-04-16 17:04:11 +0900

    [level 1] Title: 내적, Time: 2.49 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.java
+++ 프로그래머스/lv1/70128. 내적/내적.java
@@ -1,13 +1,18 @@
 import java.util.*;
+import java.util.stream.IntStream;
+
 
 class Solution {
     public int solution(int[] a, int[] b) {
-        int answer = 0;
+//         int answer = 0;
         
-        for(int i=0 ; i<a.length ; i++){
-            answer += a[i]*b[i];
-        }
+//         for(int i=0 ; i<a.length ; i++){
+//             answer += a[i]*b[i];
+//         }
         
-        return answer;
+//         return answer;
+                
+        return IntStream.range(0, a.length).map(index -> a[index] * b[index]).sum();
+
     }
 }
