227e078b85dc655c8df11b1f22e129c3c24e86e4
laveberry <73927366+laveberry@users.noreply.github.com>
2023-05-15 21:25:33 +0900

    [level 2] Title: 피보나치 수, Time: 2.49 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -6,7 +6,7 @@ class Solution {
         F[1] = 1;
         
         for(int i=2; i<=n ; i++){
-            F[i] = F[i-1]%1234567 + F[i-2]%1234567;
+            F[i] = (F[i-1] + F[i-2])%1234567;
         }
         
         return F[n]%1234567;
