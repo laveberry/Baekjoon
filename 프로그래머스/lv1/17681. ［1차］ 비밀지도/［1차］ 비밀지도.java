841fd5632245f022849dd9d0d1651667fc06bd94
laveberry <73927366+laveberry@users.noreply.github.com>
2023-05-20 09:51:25 +0900

    [level 1] Title: [1차] 비밀지도, Time: 8.43 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,36 +1,17 @@
 class Solution {
-    public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[arr1.length];
-        
-        for(int i=0; i<arr1.length ; i++){
-            String a = "", b = "";
-            String _a = Integer.toBinaryString(arr1[i]);
-            String _b = Integer.toBinaryString(arr2[i]);
-            answer[i] = "";
-            
-            if(n-_a.length()>0){
-                for(int j=0 ; j<n-_a.length() ;j++) a+="0";
-            }
-            if(n-_b.length()>0){
-                for(int j=0 ; j<n-_b.length() ;j++) b+="0";
-            }
-            a+=_a;
-            b+=_b;
-            
-            for(int k=0 ; k<n ; k++){
-                int chk = 0;
-                int chk2 = 0;
-                
-                if(a.charAt(k)=='1') chk=1;
-                if(b.charAt(k)=='1') chk2=1;
-                
-                if(chk==0 && chk2==0) answer[i]+=" ";
-                else answer[i]+="#";
-            }
+  public String[] solution(int n, int[] arr1, int[] arr2) {
+      String[] answer = new String[n];
+      String temp;
 
-        }
+      for(int i = 0 ; i < n ; i++){
+          // 대응되는 비트 중에서 하나라도 1이면 1을 반환함. (비트 OR 연산)
+          temp = String.format("%"+n+"s", Integer.toBinaryString(arr1[i] | arr2[i]));
+          //temp = temp.substring(temp.length() - n);
+          temp = temp.replaceAll("1", "#");
+          temp = temp.replaceAll("0", " ");
+          answer[i] = temp;
+      }
 
-        return answer;
-    }
-    
+      return answer;
+  }
 }
