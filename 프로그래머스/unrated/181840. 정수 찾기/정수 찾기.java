72d4026876d7d640c2a4b7fda8457f4d76adff5e
laveberry <73927366+laveberry@users.noreply.github.com>
2023-06-26 20:14:56 +0900

    [unrated] Title: 정수 찾기, Time: 1.17 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
@@ -1,14 +1,13 @@
+import java.util.*;
+import java.util.stream.*;
+
 class Solution {
     public int solution(int[] num_list, int n) {
-        int answer = 0;
         
-        for(int num : num_list){
-            if(num==n) {
-                answer = 1;
-                break;
-            }
-        }
+        // long num = Arrays.stream(num_list).filter(i -> i==n).count();
+        // return num > 0 ? 1 : 0;
         
-        return answer;
+        return IntStream.of(num_list).anyMatch(a -> a == n) ? 1 : 0;
+        
     }
 }
