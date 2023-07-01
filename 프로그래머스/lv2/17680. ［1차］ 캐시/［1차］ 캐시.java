ca93fe3a9b02cb8febc0dfe3f459be294df94ff0
laveberry <73927366+laveberry@users.noreply.github.com>
2023-07-02 08:15:53 +0900

    [level 2] Title: [1차] 캐시, Time: 1.79 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -10,7 +10,7 @@ class Solution {
         if(cacheSize==0) return cities.length * MISS;
         
         int answer = 0;
-        LinkedList<String> que = new LinkedList<>();
+        Queue<String> que = new LinkedList<>();
         
         ///캐시크기에 따른 실행시간 측정
         for(int i=0; i<cities.length; i++){
@@ -19,7 +19,7 @@ class Solution {
                 answer+=HIT;
                 que.add(s);
             }else{
-                if(que.size()>=cacheSize) que.remove(0);
+                if(que.size()>=cacheSize) que.poll();
                 answer+=MISS; 
                 que.add(s);
             }
