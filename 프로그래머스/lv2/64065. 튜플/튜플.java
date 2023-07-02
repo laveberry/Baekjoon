1a5f718a8f2a84fd85d7a1ee49fa243e9b3cfa7a
laveberry <73927366+laveberry@users.noreply.github.com>
2023-07-02 13:31:45 +0900

    [level 2] Title: 튜플, Time: 1.08 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.java
+++ 프로그래머스/lv2/64065. 튜플/튜플.java
@@ -6,19 +6,14 @@ class Solution {
         String[] str = s.split(" , ");
         
         int[] answer = new int[str.length];
-        Map<String, Integer> map = new HashMap<>();
+        Set<String> set = new HashSet<>();
         //정렬
         Arrays.sort(str, (a,b) -> (a.length() - b.length()));
         
         for(int i=0 ; i<str.length ; i++){
             String[] a = str[i].split(",");
-            if(a.length == i+1){
-                for(int k=0; k<a.length ; k++){
-                    if(map.get(a[k])==null){
-                        answer[i] = Integer.parseInt(a[k]);
-                        map.put(a[k], 1);
-                    }
-                }
+            for(int k=0; k<a.length ; k++){
+                if(set.add(a[k])) answer[i] = Integer.parseInt(a[k]);
             }
         }
         
