b7cc89b48ec7e6263659920e17629d61d5a1e5be
laveberry <73927366+laveberry@users.noreply.github.com>
2023-07-06 20:49:17 +0900

    [unrated] Title: 문자열 곱하기, Time: 0.03 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
@@ -2,12 +2,6 @@ import java.util.*;
 
 class Solution {
     public String solution(String my_string, int k) {
-        StringBuilder sb = new StringBuilder();
-        
-        for(int i=0 ; i<k ; i++){
-            sb.append(my_string);
-        } 
-        
-        return sb.toString();
+        return my_string.repeat(k);
     }
 }
