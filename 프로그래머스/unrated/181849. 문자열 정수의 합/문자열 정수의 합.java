4847ac1087ec6a8d5618ff302c3c03e8e9d668da
Dahye Kim <73927366+laveberry@users.noreply.github.com>
2023-08-11 15:51:52 +0900

    [unrated] Title: 문자열 정수의 합, Time: 1.81 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181849. 문자열 정수의 합/문자열 정수의 합.java
+++ 프로그래머스/unrated/181849. 문자열 정수의 합/문자열 정수의 합.java
@@ -1,7 +1,10 @@
+import java.util.*;
 class Solution {
     public int solution(String num_str) {
-        return num_str.chars()  // 스트림용 문자열 분리
-                 .map(Character::getNumericValue)  // 각 문자를 해당하는 정수 값으로 변환
-                 .sum();
+        return Arrays.stream(num_str.split("")).mapToInt(Integer::parseInt).sum();
+
+        // return num_str.chars()  // 스트림용 문자열 분리
+        //          .map(Character::getNumericValue)  // 각 문자를 해당하는 정수 값으로 변환
+        //          .sum();
     }
 }
